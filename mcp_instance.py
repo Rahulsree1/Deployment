7e3b69138512b6da4a028097b89c4678c107d743
Rahul <m.rahulsree1122@gmail.com>
2025-09-17 19:06:53 +0530

    Added port as env variable

--- mcp_instance.py
+++ mcp_instance.py
@@ -1,5 +1,8 @@
 # mcp_instance.py
 from mcp.server.fastmcp import FastMCP
+import os
+
+PORT = int(os.getenv("PORT", 1729))
 
 # Create a single shared instance of FastMCP
-mcp = FastMCP("RJCGG",host="0.0.0.0", port=1729, debug=True)
+mcp = FastMCP("RJCGG",host="0.0.0.0", port=PORT, debug=True)
